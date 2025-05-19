0fa4c212339109a7a79d79910e3daffd8d7d61ee
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-05-19 12:05:22 +0900

    init

---
+++ web/src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  type Message = {
+    userId: number,
+    message: string,
+  }
+
+  const sampleMessages: Message[] = [
+    {userId: 0, message: "Hello"},
+    {userId: 1, message: "world"}
+  ]
+</script>
+
+<div>Chats</div>
+
+<ul>
+  {#each sampleMessages as message}
+    <li>{message.userId}: {message.message}</li>
+  {/each}
+</ul>
